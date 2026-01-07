2045fa7ea09c6e65547700ae775b196f68db9afa
Vishwajit-Herma <vishuherma@gmail.com>
2026-01-07 11:56:22 +0530

    implemented models

---
+++ app/models.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+class BaseModel(ABC):
+    @abstractmethod
+    def to_dict(self):
+        pass
+
+class User(BaseModel):
+    def __init__(self, user_id, name, email):
+        self._id = user_id
+        self._name = name
+        self._email = email
+
+    def __str__(self):
+        return self._name
+    
+    def __eq__(self, other):
+        return self._id == other._id
+
+    def to_dict(self):
+        return {
+            'id': self._id,
+            'name': self._name,
+            'email': self._email
+        }
+    
+class Post(BaseModel):
+    def __init__(self, post_id, user_id, title):
+        self.post_id = post_id
+        self.user_id = user_id
+        self.title = title
+
+    def to_dict(self):
+        return self.__dict__
