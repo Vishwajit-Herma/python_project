eebaf6d7cd4f3de6a65bffb56a5d2bc949384549
Vishwajit-Herma <vishuherma@gmail.com>
2026-01-07 13:46:45 +0530

    implemented main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+from api_sync import fetch_users, fetch_posts
+from api_async import fetch_all
+from processor import filter_users_with_email, map_user_names
+from storage import save_json
+from concurrency import run_threading, run_multiprocessing
+
+def main():
+    users = fetch_users()
+    posts= fetch_posts()
+
+    filtered_users = filter_users_with_email(users)
+    user_names = map_user_names(users)
+
+    save_json("data/users.json", users)
+    save_json("data/posts.json", posts)
+    save_json("data/processed_data.json", user_names)
+
+    asyncio.run(fetch_all())
+
+    run_threading()
+    run_multiprocessing()
+
+    if __name__ == "__main__":
+        main()
+
