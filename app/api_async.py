ed8c5eebb56e8bd0b951eb37ebaa3fc7477c8548
Vishwajit-Herma <vishuherma@gmail.com>
2026-01-07 12:18:09 +0530

    implemented async api

--- app/api_async.py
+++ app/api_async.py
@@ -1,1 +1,17 @@
-    
+import aiohttp
+import asyncio
+from logger import logger
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.json()
+
+
+async def fetch_all():
+    logger.info("Fetching data asynchronously")
+    async with aiohttp.ClientSession() as session:
+        users, posts = await asyncio.gather(
+            fetch(session, "https://jsonplaceholder.typicode.com/users"),
+            fetch(session, "https://jsonplaceholder.typicode.com/posts")
+        )
+        return users, posts
