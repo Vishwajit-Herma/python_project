4b77ecb922d38594c12c6f0e2b40c0eefae89b14
Vishwajit-Herma <vishuherma@gmail.com>
2026-01-09 15:28:38 +0530

    add RotatingFileHandler in logger.py

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,14 +1,23 @@
 import logging
 import os
+from logging.handlers import RotatingFileHandler
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 LOG_DIR = os.path.join(BASE_DIR, "logs")
 os.makedirs(LOG_DIR, exist_ok=True)
 
+LOG_FILE = os.path.join(LOG_DIR, "app.log")
+
 logging.basicConfig(
-    filename=os.path.join(LOG_DIR, "app.log"),
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE,
+            maxBytes=1_000_000,   # 1 MB
+            backupCount=3         # keep last 3 log files
+        )
+    ]
 )
 
 logger = logging.getLogger(__name__)
