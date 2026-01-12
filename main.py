d07e9d37f302975d80abafdb102b62dad89d20b1
Vishwajit-Herma <vishuherma@gmail.com>
2026-01-12 09:56:04 +0530

    refactor main.py to clean up unnecessary whitespace and comments

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from app.storage.async_ import save_json_async
 from app.utils.logger import logger
 
 
-
 def main():
     try:
         print("Application started")
@@ -31,8 +30,7 @@ def main():
         filtered_users = filter_users_by_email_domain(users)    
         user_names = map_user_names(users)
 
-        # -------- MULTIPROCESSING FUNCTIONALITY --------
-        
+        # -------- MULTIPROCESSING FUNCTIONALITY --------  
         users_count, posts_count = process_data_with_multiprocessing(users, posts)
 
         print(f"Users count: {users_count}")
@@ -45,7 +43,6 @@ def main():
         save_json("user_names.json", user_names)        
 
         # -------- ASYNC --------
-       
         async def async_reference_demo():
 
             raw_users, raw_posts = await fetch_all()
@@ -61,12 +58,9 @@ def main():
             await save_json_async("filtered_users_async.json",[u.to_dict() for u in filtered_users])
             await save_json_async("user_names_async.json", user_names)
 
-
         asyncio.run(async_reference_demo())
 
-
         print("Application finished successfully")
-
 
     except (APIError, DataProcessingError, StorageError) as e:
         logger.error(f"Application error: {e}")
